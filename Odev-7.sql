a93687eadc5f5ac65cfa243db10358b1eb0c6c77
Ece <122381375+eceugru@users.noreply.github.com>
2024-08-15 11:32:11 +0300

    Add files via upload

---
+++ Odev-7.sql
@@ -0,0 +1,21 @@
+SELECT rating FROM film
+GROUP BY rating;
+
+
+SELECT replacement_cost,COUNT(*) FROM film
+GROUP BY replacement_cost 
+HAVING COUNT(*) > 50;
+
+
+SELECT  COUNT(store_id) FROM customer;
+
+
+SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id 
+ORDER BY country_id DESC
+LIMIT 1;
+
+
+
+
+
